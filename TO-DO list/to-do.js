623e30404a5fc5020622b1865e923e53c42183bc
nadyungkr <dyungkr@naver.com>
2020-02-02 22:02:10 +0900

    이벤트 위임하기

--- TO-DO list/to-do.js
+++ TO-DO list/to-do.js
@@ -1,19 +1,30 @@
 $(document).ready(function(){
-//checked 추가하기
-    $("li").on('click',function(){
+    $("#myUL").on('click',"li",function(e){
+        event.preventDefault();
         $(this).toggleClass('checked');
     });
 //add를 클릭하면 input의 val을 li 리스트로 만들기
-    $("#addBtn").click(function(){
+    $("#addBtn").on('click',function(){
         var inputval = $("#myInput").val();
         $("#myUL").append("<li>" + inputval + "</li>")
     });
+
 });
 
 //add를 클릭하면 input의 val을 li 리스트로 만들기 2번째 방법
+//span에 onclick = "appendTXT()"를 추가해야한다
 /*function appendTXT() {
     var inputval = document.getElementById("myInput").value;
     var addli = document.createElement("li");
     addli.innerHTML = inputval;
     $("ul").append(addli);
 }*/
+
+
+//checked 추가하기
+/*$("li").on('click',function(){
+    $(this).toggleClass('checked');
+});*/
+/*이 방법을 쓰면 안되는 이유 : 동적으로 생성된 요소는 이벤트 핸들러 등록이 되지 않아서 
+(이벤트 핸들러를 구현하는 시점에 생성되어 있지 않기 때문)부모 ul에게 
+이벤트를 등록하고 클릭 이벤트가 실제 일어난 곳으로 찾아가 위임되게 해야함*/
