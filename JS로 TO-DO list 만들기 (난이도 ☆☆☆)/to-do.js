40e71b29aa36b9e70ed0c1a19503caf90c06d925
nadyugnkr <dyungkr@naver.com>
2020-02-04 14:11:37 +0900

    새로운 to-do list 만들기 html 완성

---
+++ JS로 TO-DO list 만들기 (난이도 ☆☆☆)/to-do.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".myForm"); //form
+const myInput = document.querySelector("#myInput"); //input 입력창
+const pushList = document.querySelector("#pushList"); //전송
+const listContainer = document.querySelector(".listContainer"); //목록 div를 감싸는 container
+const listItems = document.querySelector(".listItems"); //목록 div
+const clearList = document.querySelector("#clearList"); //목록 비우기
+const item = myInput.nodeValue();
