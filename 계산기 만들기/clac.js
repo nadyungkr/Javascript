6d143b9367450cb6bbd8e302d9154c4962a5cb50
nadyungkr <dyungkr@naver.com>
2020-02-28 23:34:50 +0900

    계산기 만드는 계획 세우기

---
+++ 계산기 만들기/clac.js
@@ -0,0 +1,12 @@
+var input = document.getElementsByClassName("input").value();
+var LI = document.getElementsByTagName("li");
+
+// 어떻게 만들고 싶은가?
+// li를 클릭하면 input에 뜨게 하고 싶다
+// 그 다음은?
+// input에 뜬것끼리 계산을 시키고 싶다
+// 어떻게?
+// 어떠한 계산을 하는 기능에 input value 들어간 값을 모두 넣고
+// 그렇다면 계산을 출력하는 칸은?
+// html에 새로 만들어야 할 것 같아
+// 이 순서대로 한번 작성해보자 
